102a824a1553919a7f4e0b7ab05c6e005964ec55
VassilB <vassil.bakalov@gmail.com>
2015-01-24 20:06:11 -0800

    change to have max value of play when considering aces

--- blackjack.py
+++ blackjack.py
@@ -14,6 +14,8 @@ def CardValue(cardIndex):
 		return cardIndex;
 	elif (cardIndex >= 11):
 		return 10;
+	else:
+		print "fun:CardValue() Error, invalid card index {0}".format(cardIndex)
 
 def GetCardSymbol(cardIndex):
 	if (cardIndex > 1) and (cardIndex < 11):
@@ -26,6 +28,8 @@ def GetCardSymbol(cardIndex):
 		return "Q"
 	elif (cardIndex == 13):
 		return "K"
+	else:
+		print "fun:GetCardSymbol() Error, invalid card index {0}".format(cardIndex)
 
 def DealCards(numCards,playerName):
 	numAces = 0
@@ -55,12 +59,18 @@ def Eval_State(pc, dc, side, reward, numPAces, numDAces, level):
 				S_val = S_val_current + S_val
 
 			H_val = 0
-			for i in range (1,14):
+			# Number cards A,2,3,4,5,6,7,8,9
+			for i in range (1,10):
 				H_val_current, move = Eval_State(pc + CardValue(i), dc, "player", reward, numPAces+(i==1), numDAces,level+1)
 				PDSAtLevel("Level {4}:  Player hand: {0} ({1} Aces)  Dealer hand: {2} dealer ({3} Aces)".format(pc, numPAces, dc, numDAces, level),level,1)				
 				PDSAtLevel("H_val for card {0} is {1}.".format(GetCardSymbol(i),H_val_current),level,1)
 				H_val_current = probCard * float(H_val_current)
 				H_val = H_val + H_val_current
+
+			# Face cards and 10 - J,Q,K,10
+			H_val_current, move = Eval_State(pc + CardValue(10), dc, "player", reward, numPAces, numDAces,level+1)
+			H_val_current = 4 * probCard * float(H_val_current)
+			H_val = H_val + H_val_current
 
 			if (S_val > H_val):
 				return S_val, "stand"
@@ -80,11 +90,15 @@ def Eval_State(pc, dc, side, reward, numPAces, numDAces, level):
 			if (pc == dc): return 0 ,"stand"
 		else:
 			H_val = 0
-			for i in range(1,14):
+			for i in range(1,10):
 				H_val_current, move = Eval_State(pc, dc+CardValue(i), "dealer", reward, numPAces, numDAces+(i==1),level+1)
 				H_val_current = probCard * float(H_val_current)
 				H_val = H_val + H_val_current
-		return H_val, "hit"
+			H_val_current, move = Eval_State(pc, dc+CardValue(10), "dealer", reward, numPAces, numDAces, level+1)
+			H_val_current = 4 * probCard * float(H_val_current)
+			H_val = H_val + H_val_current
+			return H_val, "hit"			
+		return "Invalid code path"
 	print "Invalid code path"
 
 def PDSAtLevel(str, curr_level, target_level):
